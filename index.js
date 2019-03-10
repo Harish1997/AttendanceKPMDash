0742afe64aa79fec46b5023f5a77e41dd46427c6
Harish <harishbanugomathi@gmail.com>
2019-03-10 15:23:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: '<your-api-key>',
+    authDomain: '<your-auth-domain>',
+    databaseURL: '<your-database-url>',
+    storageBucket: '<your-storage-bucket>'
+  };
+  firebase.initializeApp(config);
+
+  // Get a reference to the storage service, which is used to create references in your storage bucket
+  var storage = firebase.storage();
